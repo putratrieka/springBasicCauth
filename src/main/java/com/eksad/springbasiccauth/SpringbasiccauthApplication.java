d5de19c0a50d306d0590aba396283cdc2de10195
putratrieka <putratrieka@gmail.com>
2019-07-10 09:50:51 +0700

    first commit

---
+++ src/main/java/com/eksad/springbasiccauth/SpringbasiccauthApplication.java
@@ -0,0 +1,13 @@
+package com.eksad.springbasiccauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasiccauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasiccauthApplication.class, args);
+	}
+
+}
